ff6275bb18e772069a04b1cb870e6227ae54bfdb
jejumiso <jejumiso@gmail.com>
2019-12-25 05:27:40 +0900

    주문시 현재position에 따라 에버리지값 조정.

--- bit/BitMex_ActionClass.cs
+++ bit/BitMex_ActionClass.cs
@@ -46,15 +46,15 @@ namespace bit.BitMex_ActionClass
             int step10_Qty; double step10_spring; double _margin10;
 
             step1_Qty = 1000; step1_spring = 3.0; _margin1 = 15.0;
-            step2_Qty = 2001; step2_spring = 8.0; _margin2 = 15.0;
-            step3_Qty = 2002; step3_spring = 15.0; _margin3 = 23.0;
-            step4_Qty = 2003; step4_spring = 30.0; _margin4 = 40.0;
-            step5_Qty = 2004; step5_spring = 30.0; _margin5 = 40.0;
-            step6_Qty = 2005; step6_spring = 55.0; _margin6 = 60.0;
-            step7_Qty = 2006; step7_spring = 80.0; _margin7 = 100.0;
-            step8_Qty = 2007; step8_spring = 140.0; _margin8 = 160.0;
-            step9_Qty = 2008; step9_spring = 180.0; _margin9 = 210.0;
-            step10_Qty = 2009; step10_spring = 250.0; _margin10 = 250.0;
+            step2_Qty = 3000; step2_spring = 8.0; _margin2 = 15.0;
+            step3_Qty = 3001; step3_spring = 15.0; _margin3 = 23.0;
+            step4_Qty = 3002; step4_spring = 30.0; _margin4 = 40.0;
+            step5_Qty = 3003; step5_spring = 30.0; _margin5 = 40.0;
+            step6_Qty = 3004; step6_spring = 55.0; _margin6 = 60.0;
+            step7_Qty = 3005; step7_spring = 80.0; _margin7 = 100.0;
+            step8_Qty = 3006; step8_spring = 140.0; _margin8 = 160.0;
+            step9_Qty = 3007; step9_spring = 180.0; _margin9 = 210.0;
+            step10_Qty = 3008; step10_spring = 250.0; _margin10 = 250.0;
 
             // hyunju3414764
             //step1_Qty = 500; step1_spring = 5.0; _margin1 = 15.0;
@@ -67,6 +67,74 @@ namespace bit.BitMex_ActionClass
             //step8_Qty = 50; step8_spring = 180.0; _margin8 = 210.0;
             //step9_Qty = 50; step9_spring = 250.0; _margin9 = 250.0;
             #endregion
+            double standard_avgCostPrice = (double)bitemex_position.avgCostPrice;
+            double modify_standard = 0;
+            if (Math.Abs(bitemex_position.currentQty) > 200000)
+            {
+                modify_standard = 800;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 150000)
+            {
+                modify_standard = 250;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 130000)
+            {
+                modify_standard = 200;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 100000)
+            {
+                modify_standard = 130;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 90000)
+            {
+                modify_standard = 100;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 80000)
+            {
+                modify_standard = 90;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 70000)
+            {
+                modify_standard = 80;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 60000)
+            {
+                modify_standard = 70;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 50000)
+            {
+                modify_standard = 50;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 40000)
+            {
+                modify_standard = 40;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 30000)
+            {
+                modify_standard = 30;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 20000)
+            {
+                modify_standard = 20;
+            }
+            else if (Math.Abs(bitemex_position.currentQty) > 10000)
+            {
+                modify_standard = 15;
+            }
+            else
+            {
+                //empty modify_standard =0
+            }
+            if (bitemex_position.currentQty > 0)
+            {
+                standard_avgCostPrice = standard_avgCostPrice - modify_standard;
+            }
+            else
+            {
+                standard_avgCostPrice = standard_avgCostPrice + modify_standard;
+            }
+
+
 
             //[1-2] setting
             iniinitial_value = bitemex_Bucketeds[0].open;
@@ -80,16 +148,18 @@ namespace bit.BitMex_ActionClass
             bitmex_order _bitmex_order;
             double price = 0.0;
 
+            
+
             double close = bitemex_Bucketeds[0].close;
             #region step1
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step1_spring, step1_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice,  close, iniinitial_value, step1_spring, step1_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step1_spring, step1_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step1_spring, step1_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -97,13 +167,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step2
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step2_spring, step2_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step2_spring, step2_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step2_spring, step2_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step2_spring, step2_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -111,13 +181,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step3
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step3_spring, step3_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step3_spring, step3_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step3_spring, step3_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step3_spring, step3_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -125,13 +195,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step4
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step4_spring, step4_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step4_spring, step4_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step4_spring, step4_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step4_spring, step4_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -139,13 +209,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step5
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step5_spring, step5_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step5_spring, step5_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step5_spring, step5_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step5_spring, step5_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -153,13 +223,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step6
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step6_spring, step6_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step6_spring, step6_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step6_spring, step6_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step6_spring, step6_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -167,13 +237,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step7
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step7_spring, step7_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step7_spring, step7_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step7_spring, step7_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step7_spring, step7_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -181,13 +251,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step8
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step8_spring, step8_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step8_spring, step8_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step8_spring, step8_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step8_spring, step8_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -195,13 +265,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step9
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step9_spring, step9_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step9_spring, step9_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step9_spring, step9_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step9_spring, step9_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -209,13 +279,13 @@ namespace bit.BitMex_ActionClass
             #endregion
             #region step10
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Sell", bitemex_position, close, iniinitial_value, step10_spring, step10_Qty);
+            _bitmex_order = get_bitmex_order("Sell", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step10_spring, step10_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
             }
             _bitmex_order = new bitmex_order();
-            _bitmex_order = get_bitmex_order("Buy", bitemex_position, close, iniinitial_value, step10_spring, step10_Qty);
+            _bitmex_order = get_bitmex_order("Buy", bitemex_position, standard_avgCostPrice, close, iniinitial_value, step10_spring, step10_Qty);
             if (_bitmex_order != null)
             {
                 list_bitmex_order.Add(_bitmex_order);
@@ -363,13 +433,14 @@ namespace bit.BitMex_ActionClass
             no++;
         }
 
-        private bitmex_order get_bitmex_order(string side, bitemex_position bitemex_position, double close, double iniinitial_value, double step_spring, int step_qty)
+        private bitmex_order get_bitmex_order(string side, bitemex_position bitemex_position, double standard_avgCostPrice, 
+            double close, double iniinitial_value, double step_spring, int step_qty)
         {
             if (side == "Buy")
             {
                 double price = iniinitial_value - step_spring;
                 price = close <= price ? close - 1.5 : price;
-                if (bitemex_position.currentQty == 0 || (bitemex_position.currentQty > 0 && bitemex_position.avgCostPrice > price) || (bitemex_position.currentQty < 0 && bitemex_position.marginCallPrice > price))
+                if (bitemex_position.currentQty == 0 || (bitemex_position.currentQty > 0 && standard_avgCostPrice > price) || (bitemex_position.currentQty < 0 && bitemex_position.marginCallPrice > price))
                 {
                     bitmex_order _bitmex_order = new bitmex_order();
                     _bitmex_order.symbol = "XBTUSD";
@@ -389,7 +460,7 @@ namespace bit.BitMex_ActionClass
             {
                 double price = iniinitial_value + step_spring;
                 price = close >= price ? close + 1.5 : price;
-                if (bitemex_position.currentQty == 0 || (bitemex_position.currentQty < 0 && bitemex_position.avgCostPrice < price) || (bitemex_position.currentQty > 0 && bitemex_position.marginCallPrice < price))
+                if (bitemex_position.currentQty == 0 || (bitemex_position.currentQty < 0 && standard_avgCostPrice < price) || (bitemex_position.currentQty > 0 && bitemex_position.marginCallPrice < price))
                 {
                     bitmex_order _bitmex_order = new bitmex_order();
                     _bitmex_order.symbol = "XBTUSD";
